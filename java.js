b86a903012605db2b89da1297b83dc2872224512
Jerkku03 <jere.hietikko@live.fi>
2024-01-08 17:42:49 +0200

    java valmis

--- java.js
+++ java.js
@@ -1,39 +1,64 @@
 
 // käyttäjän valitsema arvo
-
+document.addEventListener("DOMContentLoaded", () => {
 // tietokeneen arpoma arvo
 function tietokone() {
     const vaihtoehdot = ['kivi', 'sakset', 'paperi'];
     const random = Math.floor(Math.random() * vaihtoehdot.length);
     return (random, vaihtoehdot[random]);
-}
+};
 
 
+const endResult = document.querySelector('.endResult');
 
 // käyttäjän ja tietokoneen arvon vertaaminen
 
 function vertailu(pelaaja, tietokone) {
     if (pelaaja === 'kivi' && tietokone === 'sakset' || pelaaja === 'paperi' && tietokone === 'kivi' || pelaaja === 'sakset' && tietokone === 'paperi') {
-        return result = 'Voitit!'
+        const p = document.createElement('p');
+        p.innerText = 'Voitit!'
+        endResult.appendChild(p);
     } else if ( pelaaja === tietokone) {
-        return 'tasapeli'
+        const p = document.createElement('p');
+        p.innerText = 'tasapeli'
+        endResult.appendChild(p);
     }   else {
-        return 'hävisit'
+        const p = document.createElement('p');
+        p.innerText = 'hävisit'
+        endResult.appendChild(p);
     }
-}   
+};
 
 // pelin rakenne
 
-function peli() {
-    alert('elämiä 5')
-    elämät = 0;
-    while (elämät < 5) {
-        pelaaja = prompt('kirjoita kivi, sakset tai paperi');
-        alert(vertailu(pelaaja, tietokone()));
-        elämät + 1;
-    }
-    alert('elämät loppu')
-}
+// käyttäjän valinta 
 
-peli();
+    const kivi = document.querySelector('.kivi');
+    const sakset = document.querySelector('.sakset');
+    const paperi = document.querySelector('.paperi');
+    const nappulat = document.querySelector('.nappulat');
+//pelaaminen
+
+
+
+
+    kivi.addEventListener('click', () => {
+        const tietokoneValinta = tietokone();
+        const käyttäjä = 'kivi';
+        vertailu(käyttäjä, tietokoneValinta);
+}); 
+    sakset.addEventListener('click', () => {
+            const tietokoneValinta = tietokone();
+            const käyttäjä = 'sakset';
+            vertailu(käyttäjä, tietokoneValinta);
+});
+
+paperi.addEventListener('click', () => {
+            const tietokoneValinta = tietokone();
+            const käyttäjä = 'paperi';
+            vertailu(käyttäjä, tietokoneValinta);
+    });
+
+});
+
 
